df40941470f6724baef7ff500971b7d99e3bbd2f
iljuhenson <ilyapavluk24@gmail.com>
2021-03-30 22:06:22 +0300

    project base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "styles/index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function CalcBase() {
+    return (
+        <div className = "calc-base">
+            <CalcScreen />
+            <CalcNumpad />
+        </div>
+    )
+}
+
+function CalcScreen() {
+    return (
+        <div className = "calc-screen">
+            <CalcMarks />
+            <CalcResult />
+        </div>
+    )
+}
+
+function CalcMarks(props) {
+    return (
+        <div className = "calc-marks">
+
+        </div>
+    )
+}
+
+function CalcResult() {
+    return (
+        <div className = "clac-result">
+
+        </div>
+    )
+}
+
+function CalcNumpad() {
+    
+    
+    return (
+        <div className = "calc-numpad">
+            {drawButton(1)}
+            {drawButton(2)}
+            {drawButton(3)}
+            {drawButton(4)}
+            {drawButton(5)}
+            {drawButton(6)}
+            {drawButton(7)}
+            {drawButton(8)}
+            {drawButton(9)}
+            {drawButton(10)}
+            <CalcBack />
+            <CalcClear />
+        </div>
+    )
+}
+
+function CalcButton(props) {
+    return (
+        <button className="calc-button">{props.value}</button>
+    )
+}
+
+function CalcBack() {
+    return (
+        <button className="clac-button calc-back">Back</button>
+    )
+}
+
+function CalcClear() {
+    return (
+        <button className="clac-button clac-clear">Clear</button>
+    )
+}
+
+// vanila js functions for the project
+function drawButton(num) {
+    <CalcButton value = {num} />
+}
+
+ReactDOM.render(<CalcBase />, document.querySelector("#root"));
